24cd59fa922d690e0c5ba9f3538158d0571c5beb
Mittali Taurani <mitalitaurani@gmail.com>
2021-05-05 03:23:48 +0530

    Fix css for final page, payment page and access denied page

--- src/components/DesignComponents/AccessDenied/index.jsx
+++ src/components/DesignComponents/AccessDenied/index.jsx
@@ -16,7 +16,7 @@ const AccessDenied = () => {
       <img src={AccessDeniedError} alt="Access Denied" className="margin-bottom" />
       <h1 className="login-message margin-bottom">Please log in / sign up to view current page</h1> 
       <button type="button" className="login-btn blackBg-whiteFg-btn margin-bottom" onClick={handleLoginClick}>
-        Log in/ Sign up
+        Log in / Sign up
       </button>
       {showLogin && (
         <Modal>
